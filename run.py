6287fa1f08904c9c6355f13b5bda6ea9c2403c5a
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-03-31 16:01:25 +0100

    Print welcome and create a method to print intro.text

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from rich import print
+from rich.padding import Padding
+from rich.console import Console
+
+
+console = Console()
+"""
+Enable use of rich library for console output
+"""
+
+welcome = Padding(
+    (
+        ":airplane_departure: Welcome to your Travel Budget Planner "
+        ":money_with_wings:"
+    ),
+    1
+)
+
+
+def get_content(file):
+    """
+    Displays the introductory text about the content of this programme
+    """
+    intro = open(file, 'r')
+    content = intro.read()
+    intro.close()
+    return content
+
+
+begin = get_content('intro.txt')
+
+
+def main():
+    """
+    Main function to run the programme
+    """
+    console.print(welcome, style="bold #15E6E4", justify="center")
+    # displays the welcome message
+    console.print(begin, style="#9DE635", justify="center")
+    # displays the introductory text
+
+
+main()
