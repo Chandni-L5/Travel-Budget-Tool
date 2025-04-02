b837ff26972e5d0f02f6dd562cea9abc0976cd38
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-04-02 19:51:55 +0100

    Add Google docs integration for travel budget summary

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 from rich import print
 from rich.padding import Padding
 from rich.console import Console
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/drive.file"
+    "https://www.googleapis.com/auth/drive",
+]
+
+CREDS = Credentials.from_service_account_file("creds.json", scopes=SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+DOC = GSPREAD_CLIENT.open("travel_budget_summary")
 
 
 console = Console()
